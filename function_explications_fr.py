26090fef492e33c2bb96ab3e7788916effab9556
Alexis Brunet <albrunet2000@gmail.com>
2021-01-03 19:02:18 +0100

    Update function_explications_fr.py

--- function_explications_fr.py
+++ function_explications_fr.py
@@ -3,6 +3,6 @@ class fonction:                                     #Création de la classe fonc
     def  testSamples(numTrials, sampleSize, data):  #Définition de la fonction testSamples prenant en paramètres numTrials (int), sampleSize (int), data (tableau de float)
         Means = []                                  #Création d'un tableau vide
         for t in range(numTrials):                  #Boucle for commençant de 0 allant a numTrials-1 par pas de 1
-            Y=data.sample(sampleSize)               #Y récupère un échantillon aléatoire d'élèments à partir de sampleSize
+            Y=data.sample(sampleSize)               #Y récupère un échantillon aléatoire d'élèments de taille sampleSize
             Means.append(sum(Y)/len(Y))             #Ajout dans le tabelau de la division de la somme de Y divisé par la longuer de Y
         return Means                                #On retourne le tableau Means
