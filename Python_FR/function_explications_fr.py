1b9de84266584bdb9b8b0fca1b7e6150cc08d0d1
Alexis Brunet <albrunet2000@gmail.com>
2021-01-08 12:57:22 +0100

    Update function_explications_fr.py

--- Python_FR/function_explications_fr.py
+++ Python_FR/function_explications_fr.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class fonction:                                     #Création de la classe fonction pour toutes les fonctions utilisables pour temp.py
-    def  testSamples(numTrials, sampleSize, data):  #Définition de la fonction testsamples prenant en paramètre numTrials (int), sampleSize (int), data (tableau de float)
+    def  testSamples(numTrials, sampleSize, data):  #Définition de la fonction testSamples prenant en paramètre numTrials (int), sampleSize (int), data (tableau de float)
         Means = []                                  #Création d'un tableau vide
-        for t in range(numTrials):                  #Boucle for commençant de 0 allant a numTrials-1 par pas de 1
-            Y=data.sample(sampleSize)               #Y récupère un échantillon aléatoire d'éléments de taille SampleSize
-            Means.append(sum(Y)/len(Y))             #Ajout dans le tabelau de la division de la somme de Y divisé par la longuer de Y
+        for t in range(numTrials):                  #Boucle for commençant de 0 allant à numTrials-1 par pas de 1
+            Y=data.sample(sampleSize)               #Y récupère un échantillon aléatoire d'éléments de taille sampleSize
+            Means.append(sum(Y)/len(Y))             #Ajout dans le tabelau de la division de la somme de Y divisé par la longueur de Y
         return Means                                #On retourne le tableau Means
